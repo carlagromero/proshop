12d44103b3e4ab565437357172887c179dea1977
Carla Romero <carlagromero@gmail.com>
2021-05-12 16:42:38 -0300

    chore: update footer and header

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
-    return (
-        <footer>Footer</footer>
-    );
-}
- 
+  return (
+    <Container>
+      <Row>
+        <Col className='text-center py-3'>
+          <footer>Copyright &copy; ProShop</footer>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
 export default Footer;
