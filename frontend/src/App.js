b244f4a882ab7d0429729f990d360e129bcb00ec
Carla Romero <carlagromero@gmail.com>
2021-05-12 22:49:20 -0300

    chore: setup routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './screens/Home';
+import Product from './screens/Product';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <Home />
+          <Route exact path='/' component={Home} />
+          <Route path='/product/:id' component={Product} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
