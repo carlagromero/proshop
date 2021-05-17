f0aff1162a9bf076dc177e69afeb58fa4ec1b4a5
Carla Romero <carlagromero@gmail.com>
2021-05-17 16:29:15 -0300

    chore: add user registration and password encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,6 +21,32 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      admin: user.admin,
+      token: generateToken(user._id)
+    });
+  } else {
+    res.json(400);
+    throw new Error('Invalid user data');
+  }
+});
+
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = req.user;
 
@@ -37,4 +63,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
