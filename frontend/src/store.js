9246175740eed007064db5ac0736c04d208a4a50
Carla Romero <carlagromero@gmail.com>
2021-05-17 21:34:15 -0300

    chore: add login actions and reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,21 +3,30 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productsReducer, productDetailReducer } from './reducers/products';
 import cartReducer from './reducers/cart';
+import { userLoginReducer } from './reducers/user';
 
 const reducer = combineReducers({
   productsList: productsReducer,
   productDetail: productDetailReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  userLogin: userLoginReducer
 });
 
 const cartItems = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
 
+const userInfo = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : {};
+
 const middleware = [thunk];
 const INITIAL_STATE = {
   cart: {
     cartItems
+  },
+  userLogin: {
+    userInfo
   }
 };
 
