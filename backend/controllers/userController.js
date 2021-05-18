d4fe9b4bc456873f7152086b5273779e5b4585c0
Carla Romero <carlagromero@gmail.com>
2021-05-18 17:35:23 -0300

    chore: add update profile EP

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,4 +63,31 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, registerUser, getUserProfile };
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = req.user;
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    if (updatedUser) {
+      res.status(200).json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        admin: updatedUser.admin,
+        token: generateToken(updatedUser._id)
+      });
+    }
+  } else {
+    res.json(404);
+    throw new Error('User not found');
+  }
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile };
