d7e931748bb0feff2a96f951875779dbc75334b9
Carla Romero <carlagromero@gmail.com>
2021-05-20 15:47:37 -0300

    chore: create get order by id endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -35,4 +35,18 @@ const createOrder = asyncHandler(async (req, res) => {
   }
 });
 
-export { createOrder };
+const getOrder = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
+export { createOrder, getOrder };
