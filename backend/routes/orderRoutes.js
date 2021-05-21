af50bb89955494edb7c7c5ca29f97fd0e6c51fd7
Carla Romero <carlagromero@gmail.com>
2021-05-21 12:13:46 -0300

    chore: update to paid endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { createOrder, getOrder } from '../controllers/orderController.js';
+import {
+  createOrder,
+  getOrder,
+  updateOrderToPaid
+} from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/').post(protect, createOrder);
 router.route('/:id').get(protect, getOrder);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 export default router;
