d3aacbec2c38feed622cc377c7537d2ee0d3c8a6
Carla Romero <carlagromero@gmail.com>
2021-05-27 11:14:55 -0300

    chore: create review endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -82,10 +82,49 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      item => item.user === req.user._id
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error('Product already reviewed');
+    }
+
+    const review = {
+      user: req.user._id,
+      name: req.user.name,
+      rating: Number(rating),
+      comment
+    };
+
+    product.reviews.push(review);
+    product.numReviews = product.reviews.length;
+    product.rating =
+      product.reviews.reduce((acc, cur) => acc + cur.rating, 0) /
+      product.numReviews;
+
+    await product.save();
+
+    res.status(201);
+    res.json({ message: 'Review added' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
 export {
   getProducts,
   getProductById,
   createProduct,
   updateProduct,
-  deleteProduct
+  deleteProduct,
+  createProductReview
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,7 +4,8 @@ import {
   getProductById,
   createProduct,
   deleteProduct,
-  updateProduct
+  updateProduct,
+  createProductReview
 } from '../controllers/productController.js';
 import { admin, protect } from '../middleware/authMiddleware.js';
 
@@ -16,5 +17,6 @@ router
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 
 export default router;
