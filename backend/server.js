90c6aae022efbee360a1820a87bffb31184393d2
Carla Romero <carlagromero@gmail.com>
2021-05-28 17:57:03 -0300

    chore: prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
 app.use('/products', productsRoutes);
 app.use('/users', userRoutes);
 app.use('/orders', orderRoutes);
@@ -34,7 +30,20 @@ app.use('/upload', uploadRoutes);
 app.get('/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
 
 const __dirname = path.resolve();
+
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
